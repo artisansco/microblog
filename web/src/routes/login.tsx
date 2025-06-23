40614d252b344bdf7fe275ce536b849ec87d0557
Sparrow <benjaminthorpe19@gmail.com>
2025-06-23 14:21:02 +0000

    clean up component

--- web/src/routes/login.tsx
+++ web/src/routes/login.tsx
@@ -1,34 +1,34 @@
 import { Form } from "react-router";
 
 export default function LoginPage() {
-	return (
-		<>
-			<section className="mx-auto max-w-5xl">
-				<Form method="POST">
-					<legend className="mb-5 font-bold text-3xl">Sign In</legend>
+  return (
+    <>
+      <section className="mx-auto max-w-5xl">
+        <Form action="/login" method="POST">
+          <legend className="mb-5 font-bold text-3xl">Sign In</legend>
 
-					<fieldset className="space-y-5">
-						<label htmlFor="username" className="flex flex-col gap-1">
-							<span className="label w-fit">Username</span>
-							<input type="text" name="username" id="username" className="input" />
-						</label>
+          <fieldset className="space-y-5">
+            <label className="flex flex-col gap-1">
+              <span className="label w-fit">Username</span>
+              <input type="text" name="username" className="input" />
+            </label>
 
-						<label htmlFor="password" className="flex flex-col gap-1">
-							<span className="label w-fit">Password</span>
-							<input type="password" name="password" id="password" className="input" />
-						</label>
+            <label className="flex flex-col gap-1">
+              <span className="label w-fit">Password</span>
+              <input type="password" name="password" className="input" />
+            </label>
 
-						<label htmlFor="remember" className="flex items-center gap-1">
-							<input type="checkbox" name="remember" id="remember" className="input" />
-							<span className="label w-fit">Remember Me</span>
-						</label>
+            <label className="flex items-center gap-1">
+              <input type="checkbox" name="remember" className="input" />
+              <span className="label w-fit">Remember Me</span>
+            </label>
 
-						<button type="submit" className="btn">
-							Login
-						</button>
-					</fieldset>
-				</Form>
-			</section>
-		</>
-	);
+            <button type="submit" className="btn">
+              Login
+            </button>
+          </fieldset>
+        </Form>
+      </section>
+    </>
+  );
 }
